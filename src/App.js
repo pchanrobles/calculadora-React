64fdd77d1be8c4c2f2a6801f5b47c03b1485ecde
pchanrobles <carloescot@gmail.com>
2021-11-16 18:34:32 +0100

    calculadora

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Calculadora from './components/Calculadora'
+
+const App = () => {
+  return (
+    <div className="container text-center">
+      <h1>PWA Calculator</h1>
+      <hr />
+
+      <Calculadora />
+    </div>
+
+  )
+}
+
+export default App
